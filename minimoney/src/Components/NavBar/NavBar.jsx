8f5c7b5b39e30a2f43b58c0749a115ac558640cc
GabrielJM2023 <jardimgabriel2022@gmail.com>
2025-07-30 13:00:59 -0400

    Home
    Icones da home

--- minimoney/src/Components/NavBar/NavBar.jsx
+++ minimoney/src/Components/NavBar/NavBar.jsx
@@ -2,12 +2,47 @@ import React from 'react';
 import './NavBar.css'
 import Teste from './favicon.ico';
 
+import { VscGraph } from "react-icons/vsc";
+import { GrTransaction } from "react-icons/gr";
+import { FaMoneyBillTransfer } from "react-icons/fa6";
+import { CiBoxList } from "react-icons/ci";
+import { FaDoorOpen } from "react-icons/fa";
+
+
 function NavBar(){
   return (
     <div className='NavBar'>
         <div  className='Cabecalho'>
           <img src={Teste} className='logo'/>        
-          <h1>Minimoney</h1>
+          <div className='central'>
+            <h1>Minimoney</h1>
+          </div>          
+        </div>
+        <hr/>
+
+        <div className='menu'>
+          <VscGraph className='icone' />
+          <h1>Dashboard</h1>
+        </div>
+
+        <div className='menu'>
+          <GrTransaction  className='icone' />
+          <h1>Transação</h1>
+        </div>
+
+        <div className='menu'>
+          <FaMoneyBillTransfer  className='icone' />
+          <h1>Nova Transação</h1>
+        </div>
+
+        <div className='menu'>
+          <CiBoxList  className='icone' />
+          <h1>Categorias</h1>
+        </div>
+
+        <div className='menu'>
+          <FaDoorOpen  className='icone' />
+          <h1>Sair</h1>
         </div>
     </div>    
   );
