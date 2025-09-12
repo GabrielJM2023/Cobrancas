49963c1fe47f218273e86f7ad7c4bdf71371dd49
GabrielJM2023 <jardimgabriel2022@gmail.com>
2025-09-11 23:34:39 -0300

    Transacao

---
+++ minimoney/src/pages/Home/Transacao/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TransacaoPage = () => {
+    return (
+        <div>
+            <h1>Transações</h1>
+            <p>Bem-vindo à página de transações.</p>
+        </div>
+    );
+};
+
+export default TransacaoPage;   
