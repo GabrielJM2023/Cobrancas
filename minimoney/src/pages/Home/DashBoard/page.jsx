356bb4d1b590da1fd5ddb95872cc02fbbab7a092
GabrielJM2023 <jardimgabriel2022@gmail.com>
2026-01-14 08:18:03 -0400

    Page Dashboard

--- minimoney/src/pages/Home/DashBoard/page.jsx
+++ minimoney/src/pages/Home/DashBoard/page.jsx
@@ -5,5 +5,27 @@ import Button from "../../../Components/Button/button";
 // ================== Shape ativo do Pie ==================
 
 export default function Dashboard() {
-    <div>DashBVoar</div>
+    <div className="Card-DashBoard">
+        <div>
+            <h1>DashBoard</h1>
+            <div>
+                <labe>Data Inicial</labe>                
+                <input type="date"/>
+                <label>Data Final</label>
+                <input type="date"/>
+                <label>Categoria</label>
+                <select>
+                    <option value="">Todas</option>
+                    <option value="A">Alimentação</option>
+                    <option value="T">Transporte</option>
+                </select>
+                <label>Tipo</label>
+                <select>
+                    <option value="">Todos</option>
+                    <option value="E">Receitas</option>
+                    <option value="S">Despesas</option>
+                </select>
+            </div>
+        </div>   
+    </div>
 }
